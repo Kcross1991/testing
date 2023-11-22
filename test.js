f31bfe3335217fd36273b9df7669542b3f82cb30
root <root@Kacy>
2023-11-21 18:40:06 -0600

    test js

---
+++ test.js
@@ -0,0 +1,15 @@
+//Unit Test
+//Expect multiplication(2,3) to be a  number
+//Expect multiplication(4,r) to be an error
+//Expect multiplication (-6, -3) to be a number
+//Expect multiplication (a, b) to be an error
+// Expect concatOdds ([3, 2, 1], [9, 1, ,6, 4, 3, ,-6]) should return [ 1, 3, 9]
+//If there are multiple of the same number, only count it once.
+//If there are no odds generate a message saying that there are no odd numbers
+
+//Functional test
+// If the cart is empty the cart should page should appear on the screen with "Your cart is empty"
+// first step is place order or the update cart button  will pop up on the bottom of the screen. It also ask if you want to checkout as a guest or create an account.
+//Once that button is push it will take you to the payment page. Once you put in your information in you will push pay now.
+//The confirmation page should appear afterwards.
+//When a discount code is put in, the price of the cart should automatically change.
